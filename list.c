f6b108dce344890f0ba215ff78b6b8900d442945
GeraldEspinoza <>
2022-03-20 20:32:59 +0000

    new try

--- list.c
+++ list.c
@@ -75,8 +75,8 @@ void * prevList(List * list) {
 
 void pushFront(List * list, void * data) {
   Node* n= createNode(data);
-  if(list->head==NULL)
-  {list->head = n;}
+  if(list->tail==NULL)
+  {list->tail = n;}
   else {list->head->prev = n;}
   
 }
