a23fce6aa0b57186879bdf3b5e732621fa20183c
GeraldEspinoza <>
2022-03-20 18:44:42 +0000

    new try

--- list.c
+++ list.c
@@ -38,9 +38,12 @@ List * createList() {
 }
 
 void * firstList(List * list) {
-    if(list->head->data == NULL) 
+    if(list->head == NULL)
     {return NULL;}
-    else {return list->head->data;}
+  if(list->head != NULL)
+  {
+    return list->head->data;
+  }
 }
 
 void * nextList(List * list) {
